3ce3c8deebe44401a190998a89f3556415a02a2f
xavioo <rucahoxavi@gmail.com>
2022-02-11 18:17:42 +0200

    basic project setting config

--- tasklist/tasklist/settings.py
+++ tasklist/tasklist/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasktodo.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
