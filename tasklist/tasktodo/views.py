ffc2790e81b00639cc281f413ce4d80ce5f0935a
xavioo <rucahoxavi@gmail.com>
2022-02-11 20:24:23 +0200

    update a task

--- tasklist/tasktodo/views.py
+++ tasklist/tasktodo/views.py
@@ -39,3 +39,12 @@ def taskCreate(request):
 		serializer.save()
 
 	return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+	task = Task.objects.get(id=pk)
+	serializer = TaskSerializer(instance=task, data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
